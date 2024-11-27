9ccdd65e390737ea09937f757c01f95b8ea00e1f
Sohan688 <padhisohan10@gmail.com>
2024-11-28 02:13:13 +0530

    Update HomeController.java

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("message", "Welcome To Sen DevOps");
+        model.addAttribute("message", "Welcome To Sohan DevOps");
         return "home";
     }
 }
